31a326bb614bba3fbb564f4a7239ca59ecb8c5b4
SMLDN <sakmyapeo@gmail.com>
2022-04-03 20:22:17 +0900

    thuc luc noi cong

--- app/Cadd/Controller/NoiCongController.php
+++ app/Cadd/Controller/NoiCongController.php
@@ -23,44 +23,44 @@ class NoiCongController
         $this->r = $r;
     }
 
+    /** hiện k xài */
     public function index(ServerRequestInterface $request, ResponseInterface $response, $args)
     {
         $response = new Response();
-        $params = [
-            "data" => [
-                [
-                    "id" => 1,
-                    "name" => "Thiếu Lâm",
-                    "type" => "0",
-                    "slug" => "thieu-lam",
-                    "innerList" => [
-                        [
-                            "id" => 1,
-                            "name" => "Thiền Định Công"
-                        ],
-                        [
-                            "id" => 2,
-                            "name" => "Nhị Chỉ Thiền"
-                        ]
-                    ]
-                ],
-                [
-                    "id" => 2,
-                    "name" => "Võ Đang",
-                    "type" => "0",
-                    "slug" => "vo-dang",
-                    "innerList" => [
-                        [
-                            "id" => 1,
-                            "name" => "Lưỡng Nghi Hộ Tâm Công"
-                        ]
-                    ]
-                ]
-            ]
-        ];
+        // $params = [
+        //     "data" => [
+        //         [
+        //             "id" => 1,
+        //             "name" => "Thiếu Lâm",
+        //             "type" => "0",
+        //             "slug" => "thieu-lam",
+        //             "innerList" => [
+        //                 [
+        //                     "id" => 1,
+        //                     "name" => "Thiền Định Công"
+        //                 ],
+        //                 [
+        //                     "id" => 2,
+        //                     "name" => "Nhị Chỉ Thiền"
+        //                 ]
+        //             ]
+        //         ],
+        //         [
+        //             "id" => 2,
+        //             "name" => "Võ Đang",
+        //             "type" => "0",
+        //             "slug" => "vo-dang",
+        //             "innerList" => [
+        //                 [
+        //                     "id" => 1,
+        //                     "name" => "Lưỡng Nghi Hộ Tâm Công"
+        //                 ]
+        //             ]
+        //         ]
+        //     ]
+        // ];
 
-
-        $schoolCollection = School::with("inner")->get();
+        $schoolCollection = School::with("inner")->orderBy("order_num")->get();
         $schoolList = CollectionUtil::toArrayCamel($schoolCollection, [], ["inner"]);
         // dd($schoolList);
 
@@ -69,6 +69,116 @@ class NoiCongController
 
         $response->getBody()->write($this->view->renderToString("noicong.html", $params));
         return $response;
+    }
+
+    private function getInnerDetail($detail){
+        $thucLuc = "Bất Kham Nhất Kích";
+        if($detail->point > 0){
+            $thucLuc = "Sơ Học Sạ Luyện";
+        }
+
+        if($detail->point > 10){
+            $thucLuc = "Sơ Khuy Môn Kính";
+        }
+
+        if($detail->point > 15){
+            $thucLuc = "Lực Hữu Tiểu Thành";
+        }
+
+        if($detail->point > 20){
+            $thucLuc = "Giá Khinh Tựu Thực";
+        }
+
+        if($detail->point > 25){
+            $thucLuc = "Dung Hội Quán Thông";
+        }
+
+        if($detail->point > 30){
+            $thucLuc = "Lô Hỏa Thuần Thanh";
+        }
+
+        if($detail->point > 35){
+            $thucLuc = "Kỳ Tài Xuất Chúng";
+        }
+
+        if($detail->point > 40){
+            $thucLuc = "Thần Hồ Kỳ Kỹ";
+        }
+
+        if($detail->point > 45){
+            $thucLuc = "Xuất Thần Nhập Hóa";
+        }
+
+        if($detail->point > 50){
+            $thucLuc = "Ngạo Thị Quần Hùng";
+        }
+
+        if($detail->point > 55){
+            $thucLuc = "Đăng Phong Tạo Cực";
+        }
+
+        if($detail->point > 60){
+            $thucLuc = "Vô Dữ Luận Tỉ";
+        }
+
+        if($detail->point > 65){
+            $thucLuc = "Sở Hướng Phí Mị";
+        }
+
+        if($detail->point > 70){
+            $thucLuc = "Nhất Đại Tôn Sư";
+        }
+
+        if($detail->point > 75){
+            $thucLuc = "Thần Công Cái Thế";
+        }
+
+        if($detail->point > 80){
+            $thucLuc = "Cử Thế Vô Song";
+        }
+
+        if($detail->point > 85){
+            $thucLuc = "Kinh Thế Hãi Tục";
+        }
+
+        if($detail->point > 90){
+            $thucLuc = "Kinh Thiên Động Địa";
+        }
+
+        if($detail->point > 95){
+            $thucLuc = "Chấn Cổ Thước Kim";
+        }
+
+        if($detail->point > 100){
+            $thucLuc = "Siêu Phàm Nhập Thánh";
+        }
+
+        if($detail->point > 105){
+            $thucLuc = "Uy Trấn Hoàn Vũ";
+        }
+
+        if($detail->point > 110){
+            $thucLuc = "Vô Tiền Khoáng Hậu";
+        }
+
+        if($detail->point > 115){
+            $thucLuc = "Thiên Nhân Hợp Nhất";
+        }
+
+        if($detail->point > 120){
+            $thucLuc = "Thâm Tàng Bất Lộ";
+        }
+
+        if($detail->point > 135){
+            $thucLuc = "Thâm Bất Khả Trắc";
+        }
+
+        if($detail->point > 150){
+            $thucLuc = "Phản Phác Quy Chân";
+        }
+
+        $detail["thucLuc"] = $thucLuc;
+        return $detail;
     }
 
     public function detail(ServerRequestInterface $request, ResponseInterface $response, $args)
@@ -80,20 +190,15 @@ class NoiCongController
         }
         $response = new Response();
         if (Str::notEmpty($inner)) {
-            $schoolCollection = School::with("inner")->orderBy("id")->get();
+            $schoolCollection = School::with("inner")->orderBy("order_num")->get();
             $schoolList = CollectionUtil::toArrayCamel($schoolCollection, [], ["inner"]);
     
             $inner = Inner::with(["detail", "school"])->where("slug", "=", $inner)->orderBy("id")->firstOrFail();
             if ($inner != null && $level <= count($inner->detail)) {
-                $detail = $inner->detail[$level - 1];
+                $detail = $this->getInnerDetail($inner->detail[$level - 1]);
                 $effect = json_decode($inner->effect, false);
                 $effectExt = json_decode($inner->effectExt, false);
                 $currentLevel = $detail->level;
-                // dd($effectExt);
-                // dd($inner->effectExt);
-                // dd($effect);
-
-                // dd($);
                 $response->getBody()->write($this->view->renderToString("noicong/detail.html", [
                     "detail" => $detail,
                     "inner" => $inner,
--- db/seed/School.php
+++ db/seed/School.php
@@ -17,127 +17,181 @@ class School extends AbstractSeed
     {
         $data = [
             [
+                "id" => 1,
                 "name" => "Thiếu Lâm",
                 "type" => "0",
                 "slug" => "thieu-lam",
+                "order_num" => 1
             ],
             [
+                "id" => 2,
                 "name" => "Võ Đang",
                 "type" => "0",
                 "slug" => "vo-dang",
-                
+                "order_num" => 2
             ],
             [
+                "id" => 3,
                 "name" => "Nga My",
                 "type" => "0",
                 "slug" => "nga-my",
+                "order_num" => 3
             ],
             [
+                "id" => 4,
                 "name" => "Cái Bang",
                 "type" => "0",
                 "slug" => "cai-bang",
+                "order_num" => 4
             ],
             [
+                "id" => 5,
                 "name" => "Đường Môn",
                 "type" => "0",
                 "slug" => "duong-mon",
+                "order_num" => 5
             ],
             [
+                "id" => 6,
                 "name" => "Cực Lạc Cốc",
                 "type" => "0",
                 "slug" => "cuc-lac-coc",
+                "order_num" => 6
             ],
             [
+                "id" => 7,
                 "name" => "Cẩm Y Vệ",
                 "type" => "0",
                 "slug" => "cam-y-ve",
+                "order_num" => 7
             ],
             [
+                "id" => 8,
                 "name" => "Quân Tử Đường",
                 "type" => "0",
                 "slug" => "quan-tu-duong",
+                "order_num" => 8
             ],
             [
+                "id" => 9,
                 "name" => "Minh Giáo",
                 "type" => "0",
                 "slug" => "minh-giao",
+                "order_num" => 9
             ],
             [
+                "id" => 10,
                 "name" => "Thiên Sơn",
                 "type" => "0",
                 "slug" => "thien-son",
+                "order_num" => 10
             ],
             // -----
             [
+                "id" => 11,
                 "name" => "Kim Châm Thẩm Gia",
                 "type" => "1",
                 "slug" => "kim-cham-tham-gia",
+                "order_num" => 11
             ],
             [
+                "id" => 12,
                 "name" => "Di Hoa Cung",
                 "type" => "1",
                 "slug" => "di-hoa-cung",
+                "order_num" => 12
             ],
             [
+                "id" => 13,
                 "name" => "Từ Gia Trang",
                 "type" => "1",
                 "slug" => "tu-gia-trang",
+                "order_num" => 13
             ],
             [
+                "id" => 14,
                 "name" => "Vô Căn Môn",
                 "type" => "1",
                 "slug" => "vo-can-mon",
+                "order_num" => 14
             ],
             [
+                "id" => 15,
                 "name" => "Đào Hoa Đảo",
                 "type" => "1",
                 "slug" => "dao-hoa-dao",
+                "order_num" => 15
+            ],
+            [
+                "id" => 25,
+                "name" => "Vạn Thú Sơn Trang",
+                "type" => "1",
+                "slug" => "van-thu-son-trang",
+                "order_num" => 16
             ],
             // ----
             [
+                "id" => 16,
                 "name" => "Huyết Đao Môn",
                 "type" => "2",
                 "slug" => "huyet-dao-mon",
+                "order_num" => 17
             ],
             [
+                "id" => 17,
                 "name" => "Cổ Mộ",
                 "type" => "2",
                 "slug" => "co-mo",
+                "order_num" => 18
             ],
             [
+                "id" => 18,
                 "name" => "Trường Phong Tiêu Cục",
                 "type" => "2",
                 "slug" => "truong-phong-tieu-cuc",
+                "order_num" => 19
             ],
             [
+                "id" => 19,
                 "name" => "Niệm La Bá",
                 "type" => "2",
                 "slug" => "niem-la-ba",
+                "order_num" => 20
             ],
             [
+                "id" => 20,
                 "name" => "Thần Thủy Cung",
                 "type" => "2",
                 "slug" => "than-thuy-cung",
+                "order_num" => 21
             ],
             [
+                "id" => 21,
                 "name" => "Hoa Sơn",
                 "type" => "2",
                 "slug" => "hoa-son",
+                "order_num" => 22
             ],
             [
+                "id" => 22,
                 "name" => "Ngũ Tiên",
                 "type" => "2",
                 "slug" => "ngu-tien",
+                "order_num" => 23
             ],
             [
+                "id" => 23,
                 "name" => "Đạt Ma",
                 "type" => "2",
                 "slug" => "dat-ma",
+                "order_num" => 24
             ],
             [
+                "id" => 24,
                 "name" => "Thần Cơ Doanh",
                 "type" => "2",
                 "slug" => "than-co-doanh",
+                "order_num" => 25
             ],
             //---
             [
@@ -145,6 +199,7 @@ class School extends AbstractSeed
                 "name" => "Giang Hồ",
                 "type" => "3",
                 "slug" => "giang-ho",
+                "order_num" => 99
             ],
         ];
         $table = $this->table("school");
